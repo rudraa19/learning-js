b158b62c0c795e6a6afebbf5fc4d561ac8d4f8d5
rudraa19 <138957555+rudraa19@users.noreply.github.com>
2024-05-09 16:50:08 +0000

    01_basics updated

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -30,3 +30,41 @@ console.log(booleanIsLoggedIn);
         "" -> false
         "rudra" -> true
 */
+
+
+
+
+// ============================== OPERATIONS ========================================
+
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2/2)
+console.log(2**2)       // <- 2^2 (works as power)
+console.log(2%2)
+
+
+let str1 = "Hello"
+let str2 = " Rudra"
+let str3 = str1 + str2
+console.log(str3);
+
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+
+let num1, num2, num3
+num1 = num2 = num3 = 3;
+
+let gameCounter = 33
+gameCounter++;              //  <- postfix
+++gameCounter;              //  <- prefix
+console.log(gameCounter)
