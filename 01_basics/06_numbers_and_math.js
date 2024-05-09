03dbba64acfee843a744d9b9bbff37b11c72c637
rudraa19 <138957555+rudraa19@users.noreply.github.com>
2024-05-09 19:52:57 +0000

    01_basics updated

--- 01_basics/06_numbers_and_math.js
+++ 01_basics/06_numbers_and_math.js
@@ -1,30 +1,30 @@
 const score = 400
-// console.log(score);
+console.log(score);
 
 const balance = new Number(100)
-// console.log(balance);
+console.log(balance);
 
-// console.log(balance.toString().length);
-// console.log(balance.toFixed(2));
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
 
 const otherNumber = 123.8966
 
-// console.log(otherNumber.toPrecision(4));
+console.log(otherNumber.toPrecision(4));
 
 const hundreds = 1000000;
-// console.log(hundreds.toLocaleString('en-IN'));
+console.log(hundreds.toLocaleString('en-IN'));
 
 
 
 // ===================================== MATHS ======================================
 
-// console.log(Math);
-// console.log(Math.abs(-4));
-// console.log(Math.round(4.6));
-// console.log(Math.ceil(4.2));
-// console.log(Math.floor(4.9));
-// console.log(Math.min(5, 2, 3, 4));
-// console.log(Math.max(1, 9, 3, 4));
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.9));
+console.log(Math.min(5, 2, 3, 4));
+console.log(Math.max(1, 9, 3, 4));
 
 console.log(Math.random());     // <- Range from 0 to 1
 console.log((Math.random()*10) + 1);
