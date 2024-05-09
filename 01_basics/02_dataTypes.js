104cd817e19c7d021fddef4425d4df7d9e3c925b
rudraa19 <138957555+rudraa19@users.noreply.github.com>
2024-05-09 14:54:09 +0000

    01_basics updated

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict";       // <- Treats all code as newer version of JS
+
+// alert("Hello")       // <- we're using node.js not browser
+
+let name = "Rudra"
+let age = 18
+let isLoggedIn = false
+
+
+/*
+    number -> 2^53
+    bigint
+    string -> ""
+    boolean -> true or false
+    null-> stand alone value
+    undefined 
+    symbol-> for uniqueness
+    object
+*/
+
+
+console.log(typeof undefined);      // <- OUTPUT: undefined
+console.log(typeof null);           // <- OUTPUT: object
